b0db6372d40d87060e0e17a3d29d2d5cdfdfcf96
SamuelD13 <samueldaoud137@gmail.com>
2023-12-21 13:02:47 +0100

    running server

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import flask
 from transmitter import Transmitter
 import subprocess
+import time
 
 app = flask.Flask(__name__)
 transmitter = Transmitter()
@@ -10,14 +11,35 @@ def index():
     return flask.render_template('index.html')
 
 @app.route('/prompt', methods=['POST'])
-def prompt():
+def prompt() :
     if flask.request.method == 'POST':
         question = flask.request.form['user_input']
-        with open("input_txt", 'w') as file:
+
+        with open("input_txt", 'w') as file :
             file.write(question)
-        subprocess.run(['sbatch', "transmit.batch"])
-        with open("transmit-output.log", 'r') as file:
+
+        try :
+            result = subprocess.run(['sbatch', "transmit.sbatch"], check=True, stdout=subprocess.PIPE, text=True)
+            id = result.stdout.split()[-1]
+            print("Sbatch script executed successfully. Job ID :", id)
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing sbatch script: {e}")
+
+        while True :
+            try :
+                status = subprocess.check_output(['squeue', '-j', id], text=True)
+                print("Job status:", status)
+                if id not in status.split() :
+                    print("Job is done")
+                    break
+            except subprocess.CalledProcessError :
+                print("Error searching for the job")
+                break
+            time.sleep(10)
+
+        with open("output/answer.txt", 'r') as file:
             answer = file.read()
+
         return flask.render_template('index.html', output=answer)
 
 if __name__ == '__main__':
